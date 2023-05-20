dbe79ddd91e58a91c6e13d65a32c14da90ef49ee
trantuananh21 <109894389+trantuananh21@users.noreply.github.com>
2023-05-20 08:15:26 +0200

    upload

--- Basic/lesson05/index.py
+++ Basic/lesson05/index.py
@@ -1,159 +1,159 @@
 # Phần 1
 # 1.
-# firstName = input('Your First name: ')
-# lastName = input('Your Last name: ')
-# fullName = firstName + ' ' + lastName
+firstName = input('Your First name: ')
+lastName = input('Your Last name: ')
+fullName = firstName + ' ' + lastName
 
-# print('Your full name is:', fullName)
+print('Your full name is:', fullName)
 
 # 2.
-# yourInput = input('Your Input: ')
+yourInput = input('Your Input: ')
 
-# print('Capitalized:', yourInput.upper())
+print('Capitalized:', yourInput.upper())
 
 
 # 3.
-# num1 = float(input('Input your number: '))
-# square = num1**2
+num1 = float(input('Input your number: '))
+square = num1**2
 
-# print('Squared input:', square)
+print('Squared input:', square)
 
 
 # 4.
-# r = int(input("Input circle's radius: "))
+r = int(input("Input circle's radius: "))
 
-# import turtle as t
-# t.circle(r)
-# t.mainloop()
+import turtle as t
+t.circle(r)
+t.mainloop()
 
 
 # Phần 2
 # 1.
-# for i in range(3,13):
-#     print(i, end=' ')
+for i in range(3,13):
+    print(i, end=' ')
 
 # 2.
-# num2 = int(input('Input your number: '))
+num2 = int(input('Input your number: '))
 
-# for i in range(0,num2+1):
-#     print(i, end=' ')
+for i in range(0,num2+1):
+    print(i, end=' ')
 
 # 3.
-# num3 = int(input('Input a number: '))
+num3 = int(input('Input a number: '))
 
-# for i in range(0, num3+1):
-#     if i % 2 != 0:
-#         print(i, end=' ')        
+for i in range(0, num3+1):
+    if i % 2 != 0:
+        print(i, end=' ')        
 
 
 # 4.
-# num4 = int(input('Input your number: '))
-# angle = 360/num4
+num4 = int(input('Input your number: '))
+angle = 360/num4
 
-# for i in range(num4):
-#     import turtle as t
-#     t.forward(100)
-#     t.left(angle)
+for i in range(num4):
+    import turtle as t
+    t.forward(100)
+    t.left(angle)
 
-# t.mainloop()
+t.mainloop()
 
 
 # Phần 3
 # 1.
-# num5 = int(input('Input a number: '))
+num5 = int(input('Input a number: '))
 
-# if num5 > 13:
-#     print('This number is larger than 13')
-# else:
-#     print('This number is not larger than 13')
+if num5 > 13:
+    print('This number is larger than 13')
+else:
+    print('This number is not larger than 13')
 
 # 2.
-# num6 = int(input('Input a number: '))
+num6 = int(input('Input a number: '))
 
-# if num6 % 2 == 0:
-#     print('This number is even')
-# elif num6 % 2 != 0:
-#     print('This number is not even')
+if num6 % 2 == 0:
+    print('This number is even')
+elif num6 % 2 != 0:
+    print('This number is not even')
 
 # 3.
-# print('Input a month: ', end='')
-# month = int(input())
+print('Input a month: ', end='')
+month = int(input())
 
-# if month == 1 or month == 3 or month == 5 or\
-#    month == 7 or month == 8 or month == 10 or month == 12:
-#   print('This month has 31 days')
-# elif month == 4 or month == 6 or month == 9 or month == 11:
-#   print('This month has 30 days')
-# elif month == 2:
-#   print('This month has 28 or 29 days')
-# else:
-#   print('Invalid month')
+if month == 1 or month == 3 or month == 5 or\
+   month == 7 or month == 8 or month == 10 or month == 12:
+  print('This month has 31 days')
+elif month == 4 or month == 6 or month == 9 or month == 11:
+  print('This month has 30 days')
+elif month == 2:
+  print('This month has 28 or 29 days')
+else:
+  print('Invalid month')
 
 # Phần 4
 # 1.
-# username = input('Username: ')
-# password = input('Password: ')
-# email = input('Email: ')
+username = input('Username: ')
+password = input('Password: ')
+email = input('Email: ')
 
-# print('Registered successfully.')
+print('Registered successfully.')
 
 # 2.
-# userName = input('Username: ')
-# passWord = input('Password: ')
+userName = input('Username: ')
+passWord = input('Password: ')
 
-# while True:
-#   repeatPass = input('Repeat password: ')
-#   if repeatPass == passWord:
-#     break
-#   else:
-#     print('Passwords not match. Please input again.')
+while True:
+  repeatPass = input('Repeat password: ')
+  if repeatPass == passWord:
+    break
+  else:
+    print('Passwords not match. Please input again.')
 
-# email1 = input('Email: ')
-# print('\nRegistered successfully.')
+email1 = input('Email: ')
+print('\nRegistered successfully.')
 
 # 3.
-# Username = input('Username: ')
+Username = input('Username: ')
 
-# # Check đủ hơn 8 chữ số trong một mật khẩu
-# while True:
+# Check đủ hơn 8 chữ số trong một mật khẩu
+while True:
 
-#   print('\nPassword: ', end='')
-#   password1 = input()
+  print('\nPassword: ', end='')
+  password1 = input()
   
-#   has_letter = False
-#   has_digits = False
-#   for ch in password1:
-#     if (ch > 'a' and ch < 'z') or (ch > 'A' and ch < 'Z'): 
-#       has_letter = True
-#     if ch > '0' and ch < '9':  
-#       has_digits = True
+  has_letter = False
+  has_digits = False
+  for ch in password1:
+    if (ch > 'a' and ch < 'z') or (ch > 'A' and ch < 'Z'): 
+      has_letter = True
+    if ch > '0' and ch < '9':  
+      has_digits = True
   
-#   if has_letter and has_digits and len(password1) >= 8:
-#     break
-#   else:
-#     print('Invalid password. Please input again.', end='')
+  if has_letter and has_digits and len(password1) >= 8:
+    break
+  else:
+    print('Invalid password. Please input again.', end='')
 
 
-# # Check hai mật khẩu có trùng nhau không
-# while True:
-#   print('Repeat password: ', end='')
-#   if input() == password1:
-#     break
-#   else:
-#     print('Passwords not match. Please input again.')
+# Check hai mật khẩu có trùng nhau không
+while True:
+  print('Repeat password: ', end='')
+  if input() == password1:
+    break
+  else:
+    print('Passwords not match. Please input again.')
 
-# # Check gmail có hợp lệ hay không
-# while True:
+# Check gmail có hợp lệ hay không
+while True:
 
-#   print('\nEmail: ', end='')
-#   email = input()
+  print('\nEmail: ', end='')
+  email = input()
 
-#   if ('.' in email) and ('@' in email):
-#     break
-#   else:
-#     print('Invalid email. Please input again.', end='')
+  if ('.' in email) and ('@' in email):
+    break
+  else:
+    print('Invalid email. Please input again.', end='')
 
-# print('\nRegistered successfully.')
+print('\nRegistered successfully.')
 
 
 # Phần 5
