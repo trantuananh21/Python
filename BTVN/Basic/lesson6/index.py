feb4a2c42b7b8e2374e8b7b005a89cb5ba85d984
trantuananh21 <109894389+trantuananh21@users.noreply.github.com>
2023-05-27 09:09:46 +0200

    upload

--- BTVN/Basic/lesson6/index.py
+++ BTVN/Basic/lesson6/index.py
@@ -1,57 +1,82 @@
 # Bài 1
-# arr = [0,1,2,3,4,5,6,7,8,9]
+arr = [0,1,2,3,4,5,6,7,8,9]
 
-# addTwoList = list()
-# mulTwoList = list()
+addTwoList = list()
+mulTwoList = list()
 
-# for i in arr:
-#     addTwo = i + 2
-#     addTwoList.append(addTwo)
-#     mulTwo = i * 2
-#     mulTwoList.append(mulTwo)
+for i in arr:
+    addTwo = i + 2
+    addTwoList.append(addTwo)
+    mulTwo = i * 2
+    mulTwoList.append(mulTwo)
 
-# shiftTwoList = arr[2:] + arr[:2]
+shiftTwoList = arr[2:] + arr[:2]
 
-# print('Original array: ', arr)
-# print('Add 2: ', addTwoList)
-# print('Multipy 2: ', mulTwoList)
-# print('Shift 2: ', shiftTwoList)
+print('Original array: ', arr)
+print('Add 2: ', addTwoList)
+print('Multipy 2: ', mulTwoList)
+print('Shift 2: ', shiftTwoList)
 
 
 # Bài 2
-# arr2 = ['l', 'o', 'o', 'h', 'c', 'S', ' ', 'y', 'g', 'o', 'l',
-#         'o', 'n', 'h', 'c', 'e', 'T', ' ', 'X', 'd', 'n', 'i', 'M']
+arr2 = ['l', 'o', 'o', 'h', 'c', 'S', ' ', 'y', 'g', 'o', 'l',
+        'o', 'n', 'h', 'c', 'e', 'T', ' ', 'X', 'd', 'n', 'i', 'M']
 
-# arr2.reverse()
-# print(arr2)
+arr2.reverse()
+print(arr2)
 
 # Bài 3
-# num = int(input('Input a positive number: '))
+num = int(input('Input a positive number: '))
 
-# num1, num2 = 1, 1
-# count = 0
+num1, num2 = 1, 1
+count = 0
 
-# if num <= 0:
-#     print('It must bigger than 0')
-# elif num == 1:
-#     print('First 1 Fibonacci number(s): 1')
-# elif num > 1:
-#     print(f'First {num} Fibonacci number(s):')
-#     while count < num:
-#        print(num1, end=' ')
-#        nth = num1 + num2
-#        num1 = num2
-#        num2 = nth
-#        count += 1
+if num <= 0:
+    print('It must bigger than 0')
+elif num == 1:
+    print('First 1 Fibonacci number(s): 1')
+elif num > 1:
+    print(f'First {num} Fibonacci number(s):')
+    while count < num:
+       print(num1, end=' ')
+       nth = num1 + num2
+       num1 = num2
+       num2 = nth
+       count += 1
 
 # Bài 4
-# menu = (('Ribeye Steak', 30.5), ('Potato Salad', 5), ('Sparkling Wine', 7), 
-#         ('Smoked Salmon', 12), ('Chicken Soup', 12.5), ('Tiramisu Cake', 4.5))
-# print(menu[0])
+menu = (('Ribeye Steak', 30.5), ('Potato Salad', 5), ('Sparkling Wine', 7), 
+        ('Smoked Salmon', 12), ('Chicken Soup', 12.5), ('Tiramisu Cake', 4.5))
+
+sum = 0
+count = 0
+lPriceMoreThanAvg = []
+
+for i in menu:
+    food = i[0]
+    price = i[1]
+    sum += price
+
+    count += 1
+    print(f'''
+    Item {count}: {food}
+    Price of item {count}: {price}
+    ''')
+
+avg = sum / count
+
+print('Number of items: ', count)
+
+for x in menu:
+    if x[1] > avg:
+        lPriceMoreThanAvg.append(x)
+
+print('Avarage price: ', avg)
+print('Items more than avarage: ', lPriceMoreThanAvg)
 
 
 # Bài 5
-# inputStr = input('Write a sentence: ')
-# wordList = inputStr.split(' ')
-# length = len(wordList)
-# print('Number of unique words:', length)
+inputStr = input('Write a sentence: ')
+wordList = inputStr.split(' ')
+length = len(wordList)
+print('Number of unique words:', length)
