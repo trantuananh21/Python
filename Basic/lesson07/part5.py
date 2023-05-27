25d2b9400405e86a0695e05bfe2e8366de9f5b2a
trantuananh21 <109894389+trantuananh21@users.noreply.github.com>
2023-05-27 16:09:03 +0200

    upload

--- Basic/lesson07/part5.py
+++ Basic/lesson07/part5.py
@@ -18,3 +18,10 @@ print(f'''
 Most populated dist.: {popOfDist.index(max)}
 Least populated dist.: {popOfDist.index(min)}
 ''')
+
+# 3
+print(f'''
+Name of:
+Most populated dist.: {nameOfDist[int(popOfDist.index(max)):int(popOfDist.index(max))+1]}
+Least populated dist.: {nameOfDist[int(popOfDist.index(min)):int(popOfDist.index(min))+1]}
+''')
