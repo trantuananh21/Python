d7fec5a153631178a87266d5d39b3d84f72ff69f
trantuananh21 <109894389+trantuananh21@users.noreply.github.com>
2023-07-15 15:50:22 +0200

    upload

--- Basic/lesson14/ex04.py
+++ Basic/lesson14/ex04.py
@@ -1,24 +1,22 @@
-my_list = ['Pho', 'Bun Bo', 'Goi Cuon', 'Bun Cha']
-my_order = []
+orders = []
 
-print('== Welcome to MindX Restaurant ==')
+print('= Welcome to MindX Restaurant ==')
 
-order = input('Please choose a dish: ')
-if order in my_list:
-    my_order.append(order)
-    order02 = input('Great choice! Anything else? (y/n): ')
-    if order02 == 'y':
-        order = input('Please choose a dish: ')
-        if order not in my_order:
-            my_order.append(order)
-        else:
-            order03 = input('You chose this already.')
-        print(f'''
-              Well done. Your order:
-              {my_order}
-              ''')
-    elif order02 == 'n':
-        print(f'''
-              Well done. Your order is:
-              {my_order}
-              ''')
+while True:
+  print('Please choose a dish: ', end='')
+  dish = input()
+
+  if dish not in orders: 
+    orders.append(dish)
+    print('Great choice! ', end='')
+  else:
+    print('You chose this already. ', end='')
+
+  print('Anything else? (y/n): ', end='')
+  choice = input()
+  if choice == 'n':
+    break
+
+print('Well done! Your order:')
+for dish in orders:
+  print('-', dish)
