661e1ecd3af6b0ab16567d26174e5150f3152d80
trantuananh21 <109894389+trantuananh21@users.noreply.github.com>
2023-08-27 23:34:27 +0700

    upload

---
+++ Advanced/lesson04/game.py
@@ -0,0 +1,20 @@
+import pygame
+# import random
+
+# KHOI TAO GAME
+pygame.init()
+
+# CAI DAT MAN HINH
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Game tre con')
+
+# MAU SAC
+white = (255, 255, 255)
+green = (0, 256, 0)
+red = (255, 0, 0)
+
+# TOC DO KHUNG HINH
+clock = pygame.time.Clock()
+fps =   10
+
