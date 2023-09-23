72d0027ad4430744e92dcf86bd690d3470450f20
trantuananh21 <109894389+trantuananh21@users.noreply.github.com>
2023-09-23 08:03:31 +0200

    upload

--- Advanced/lesson06/index.py
+++ Advanced/lesson06/index.py
@@ -1,2 +1,13 @@
 # Cơ sở dữ liệu là nơi để lưu trữ dữ liệu
 
+import sqlite3
+conn = sqlite3.connect('/Users/macbook/Documents/Python/Python/Advanced/lesson06/data.db')
+c = conn.cursor()
+c.execute('''
+    create table employ_info(
+          employ_id int,
+          first_name varchar,
+          last_name varchar,
+          primary key(employ_id)
+    )
+''')
